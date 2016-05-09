628f0ba496d275b4ac801530624f7e9abe3387fc
hjfbynara <hjferrara@bynara.com>
2016-05-09 07:47:52 -0400

    added ability to config mpm_worker/event via envs. Added ability to conifgure ProxyPass via env

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM broadinstitute/openidc-baseimage:latest
 
 ADD site.conf stackdriver.conf /etc/apache2/sites-available/
+ADD mpm_event.conf mpm_worker.conf /etc/apache2/mods-available/
 ADD override.sh /etc/apache2/
 
 RUN a2enmod authnz_ldap
--- override.sh
+++ override.sh
@@ -93,6 +93,63 @@ if [ -z "$LDAP_CACHE_TTL" ] ; then
     export LDAP_CACHE_TTL='60'
 fi
 
+# MPM EVENTS SETTINGS (could also be used for WORKER)
+
+# update SERVERLIMIT
+if [ -z "$SERVERLIMIT" ] ; then
+    export SERVERLIMIT=16
+fi
+
+# update STARTSERVERS
+if [ -z "$STARTSERVERS" ] ; then
+    export STARTSERVERS=2
+fi
+
+# update MINSPARETHREADS
+if [ -z "$MINSPARETHREADS" ] ; then
+    export MINSPARETHREADS=25
+fi
+
+# update MAXSPARETHREADS
+if [ -z "$MAXSPARETHREADS" ] ; then
+    export MAXSPARETHREADS=75
+fi
+
+# update THREADSPERCHILD
+if [ -z "$THREADSPERCHILD" ] ; then
+    export THREADSPERCHILD=25
+fi
+
+# update THREADLIMIT
+if [ -z "$THREADLIMIT" ] ; then
+    export THREADLIMIT=64
+fi
+
+# update MAXREQUESTWORKERS
+if [ -z "$MAXREQUESTWORKERS" ] ; then
+    export MAXREQUESTWORKERS=150
+fi
+
+# update MAXCONNECTIONSPERCHILD
+if [ -z "$MAXCONNECTIONSPERCHILD" ] ; then
+    export MAXCONNECTIONSPERCHILD=0
+fi
+
+# update PROXYPASS_PARAMS
+if [ -z "$PROXYPASS_PARAMS" ] ; then
+    export PROXYPASS_PARAMS=
+fi
+
+# update PROXYPASS_PARAMS2
+if [ -z "$PROXYPASS_PARAMS2" ] ; then
+    export PROXYPASS_PARAMS2=
+fi
+
+# update PROXYPASS_PARAMS3
+if [ -z "$PROXYPASS_PARAMS3" ] ; then
+    export PROXYPASS_PARAMS3=
+fi
+
 # update PROXY_PATH3
 if [ -z "$PROXY_PATH3" ] ; then
     export PROXY_PATH3=/register
